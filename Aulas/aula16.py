146d9ccbc8107c7531d7e3fe61459cdcc291ba0e
Anderson Santos <andersonacpemp@gmail.com>
2025-02-21 22:28:14 -0300

    Add aula 16

---
+++ Aulas/aula16.py
@@ -0,0 +1,20 @@
+'''
+-= Anotando algumas coisas que podem ser usadas nas tuplas =-
+
+tupla = ('Anderson', 'Cristiana', 'Pedro', 'Anelyse') => lista de exemplificação
+tupla_numeros = (1, 2, 3, 4, 5)
+for i, c in enumerate(tupla) printa o elemento e sua posição, por isso as 2 variáveis
+   A primeira printa a posição, a segunda o elemento naquela posição
+
+for i in tupla:
+    #print(i)
+
+for i in range(0, len(tupla))
+    print(tupla[i])
+
+print(sorted(tupla)) => printa a lista de forma ordenada
+print(len(tupla)) => mostra quantos elementos tem na lista
+print(tupla_numeros.count(1)) => mostra quantos elementos desse valor tem dentro da tupla
+print(tupla_numeros.index(1)) => mostra a posição que o elemento desejado se encontra
+função del() => deleta a variável
+'''
